126dc84721d4c6c5c8586a5b65d37eff1d95c9ed
Isa <codingisads@gmail.com>
2025-05-02 21:37:22 +0200

    header completed

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../Header/Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-image'>
+      <div className='blurred'>
+        <div className='title'>
+          <div>
+            <h1>sweet <br />hearts</h1>
+            <p>by micaela</p>
+            
+          <div className='subtitle'>
+          bento cakes | pasteles individuales | postre tendencia en Corea
+          </div>
+          </div>
+          
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavBar from '../../layouts/NavBar/NavBar'
 import Body from '../../layouts/Body/Body'
+import Header from '../../layouts/Header/Header'
 
 export const Home = () => {
     return (
         <div>
             <NavBar/>
-            <div style={{height: "500px"}}></div>
+            <Header></Header>
             <Body/>
         </div>
     )
