f4d1db6f2fff4d21eed48d9249750c18a9c17527
Isa <codingisads@gmail.com>
2025-05-02 12:20:39 +0200

    new project

---
+++ web-pasteles/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { PrimerComponente } from './components/PrimerComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <PrimerComponente/>
+    </div>
+
+    
+  );
+}
+
+export default App;
